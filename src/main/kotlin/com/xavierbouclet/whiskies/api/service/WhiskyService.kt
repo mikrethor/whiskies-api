51f070fc159acc282a99e99eb88fb7d785cd312a
Xavier Bouclet <mikrethor@gmail.com>
2023-03-01 05:50:54 -0500

    Convert service  to KT

--- src/main/kotlin/com/xavierbouclet/whiskies/api/service/WhiskyService.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/service/WhiskyService.kt
@@ -1,12 +1,9 @@
-package com.xavierbouclet.whiskies.api.service;
+package com.xavierbouclet.whiskies.api.service
 
-import com.xavierbouclet.whiskies.api.model.Whisky;
-import org.springframework.web.service.annotation.GetExchange;
+import com.xavierbouclet.whiskies.api.model.Whisky
+import org.springframework.web.service.annotation.GetExchange
 
-import java.util.List;
-
-public interface WhiskyService {
-
+interface WhiskyService {
     @GetExchange("/whiskies")
-    List<Whisky> loadAll();
+    fun loadAll(): List<Whisky>
 }
