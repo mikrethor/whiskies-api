519ce04e6962e0d4658bcc04d3bfebd323ac6597
Xavier Bouclet <mikrethor@gmail.com>
2023-03-01 05:36:31 -0500

    Convert exception and ExceptionHandlerAdvice to KT

--- src/main/kotlin/com/xavierbouclet/whiskies/api/exception/ElementNotFoundException.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/exception/ElementNotFoundException.kt
@@ -1,18 +1,8 @@
-package com.xavierbouclet.whiskies.api.exception;
+package com.xavierbouclet.whiskies.api.exception
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.UUID;
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.util.*
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ElementNotFoundException extends RuntimeException {
-
-    private final UUID id;
-    public ElementNotFoundException(UUID id) {
-        this.id=id;
-    }
-    public UUID getId() {
-        return id;
-    }
-}
+class ElementNotFoundException(val id: UUID) : RuntimeException()
--- src/main/kotlin/com/xavierbouclet/whiskies/api/exception/ExceptionHandlerAdvice.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/exception/ExceptionHandlerAdvice.kt
@@ -1,22 +1,20 @@
-package com.xavierbouclet.whiskies.api.exception;
+package com.xavierbouclet.whiskies.api.exception
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.net.URI;
-import java.net.URISyntaxException;
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.net.URI
+import java.net.URISyntaxException
 
 @RestControllerAdvice
-public class ExceptionHandlerAdvice {
-
-    @ExceptionHandler(ElementNotFoundException.class)
-    public ProblemDetail handlePostNotFoundException(ElementNotFoundException exception) throws URISyntaxException {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
-        problemDetail.setProperty("id", exception.getId());
-        problemDetail.setType(new URI("http://localhost:8080/problems/post-not-found"));
-        return problemDetail;
-
+class ExceptionHandlerAdvice {
+    @ExceptionHandler(ElementNotFoundException::class)
+    @Throws(URISyntaxException::class)
+    fun handlePostNotFoundException(exception: ElementNotFoundException): ProblemDetail {
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.message!!)
+        problemDetail.setProperty("id", exception.id)
+        problemDetail.type = URI("http://localhost:8080/problems/post-not-found")
+        return problemDetail
     }
 }
