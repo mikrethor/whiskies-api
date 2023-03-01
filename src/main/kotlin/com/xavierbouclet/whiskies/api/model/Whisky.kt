1b874bc538126390cc774c5937d180097956030d
Xavier Bouclet <mikrethor@gmail.com>
2023-03-01 06:02:33 -0500

    Convert entity to KT

--- src/main/kotlin/com/xavierbouclet/whiskies/api/model/Whisky.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/model/Whisky.kt
@@ -1,76 +1,26 @@
-package com.xavierbouclet.whiskies.api.model;
+package com.xavierbouclet.whiskies.api.model
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.util.*
 
 @Entity
-public class Whisky {
 
+data class Whisky(
     @Id
     @JsonProperty("id")
-    private UUID id;
+    var id: UUID?=null,
+
     @JsonProperty("Bottle")
-    private String bottle;
+    val bottle: String="",
+
     @JsonProperty("Price")
-    private String price;
+    val price: String="",
 
     @JsonProperty("Rating")
-    private String rating;
+    val rating: String="",
+
     @JsonProperty("Region")
-    private String region;
-
-    protected Whisky() {
-    }
-
-    public Whisky(UUID id, String bottle, String price, String rating, String region) {
-        this();
-        this.id = id;
-        this.bottle = bottle;
-        this.price = price;
-        this.rating = rating;
-        this.region = region;
-    }
-
-    public UUID id() {
-        return id;
-    }
-
-    public void id(UUID id) {
-        this.id = id;
-    }
-
-    public String bottle() {
-        return bottle;
-    }
-
-    public void bottle(String bottle) {
-        this.bottle = bottle;
-    }
-
-    public String price() {
-        return price;
-    }
-
-    public void price(String price) {
-        this.price = price;
-    }
-
-    public String rating() {
-        return rating;
-    }
-
-    public void rating(String rating) {
-        this.rating = rating;
-    }
-
-    public String region() {
-        return region;
-    }
-
-    public void region(String region) {
-        this.region = region;
-    }
-}
+    val region: String=""
+)
