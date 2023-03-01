aced4c8829638919cfafad258f5fdf0bd70286b5
Xavier Bouclet <mikrethor@gmail.com>
2023-03-01 05:24:24 -0500

    Convert controller class

--- src/main/kotlin/com/xavierbouclet/whiskies/api/controller/WhiskyController.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/controller/WhiskyController.kt
@@ -1,33 +1,24 @@
-package com.xavierbouclet.whiskies.api.controller;
+package com.xavierbouclet.whiskies.api.controller
 
-import com.xavierbouclet.whiskies.api.exception.ElementNotFoundException;
-import com.xavierbouclet.whiskies.api.model.Whisky;
-import com.xavierbouclet.whiskies.api.repository.WhiskyRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.UUID;
+import com.xavierbouclet.whiskies.api.exception.ElementNotFoundException
+import com.xavierbouclet.whiskies.api.model.Whisky
+import com.xavierbouclet.whiskies.api.repository.WhiskyRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/whiskies")
-public class WhiskyController {
-
-    private final WhiskyRepository postRepository;
-
-    public WhiskyController(WhiskyRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-
+class WhiskyController(private val postRepository: WhiskyRepository) {
     @GetMapping
-    public List<Whisky> findAll() {
-        return postRepository.findAll();
+    fun findAll(): List<Whisky> {
+        return postRepository.findAll()
     }
 
     @GetMapping("/{id}")
-    public Whisky findById(@PathVariable("id") UUID id) {
-        return postRepository.findById(id).orElseThrow(()->new ElementNotFoundException(id));
+    fun findById(@PathVariable("id") id: UUID): Whisky {
+        return postRepository.findById(id).orElseThrow { ElementNotFoundException(id) }
     }
 }
