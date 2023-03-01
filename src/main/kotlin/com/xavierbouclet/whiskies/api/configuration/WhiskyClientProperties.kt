2273f8f94b7f8e3f0df1ff3817cc4acac5361488
Xavier Bouclet <mikrethor@gmail.com>
2023-03-01 05:26:28 -0500

    Convert configuration class

--- src/main/kotlin/com/xavierbouclet/whiskies/api/configuration/WhiskyClientProperties.kt
+++ src/main/kotlin/com/xavierbouclet/whiskies/api/configuration/WhiskyClientProperties.kt
@@ -1,14 +1,9 @@
-package com.xavierbouclet.whiskies.api.configuration;
+package com.xavierbouclet.whiskies.api.configuration
 
-import jakarta.validation.constraints.NotNull;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+import jakarta.validation.constraints.NotNull
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.validation.annotation.Validated
 
 @Validated
 @ConfigurationProperties(prefix = "client.whisky.service")
-public record WhiskyClientProperties(@NotNull String url) {
-}
-
-
-
-
+data class WhiskyClientProperties(val url: @NotNull String)
