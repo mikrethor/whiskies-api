c40b98b14befb3e23a3ccac04c4d3e8b679aefa8
Xavier Bouclet <mikrethor@gmail.com>
2023-06-19 21:59:45 -0400

    Fix RouterConfiguration & Handler

--- src/main/java/com/xavierbouclet/whiskies/api/configuration/RouterConfiguration.java
+++ src/main/java/com/xavierbouclet/whiskies/api/configuration/RouterConfiguration.java
@@ -9,11 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.servlet.function.EntityResponse;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import java.net.URI;
-import java.util.function.BiFunction;
 
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
@@ -26,22 +24,18 @@ public class RouterConfiguration {
                 .GET("/api/whiskies", whiskyHandler::findAll)
                 .GET("/api/whiskies/{id}",whiskyHandler::findById)
                 .onError(ElementNotFoundException.class,
-                        error())
+                        (e, req) -> {
+                            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+                            problemDetail.setProperty("id", ((ElementNotFoundException) e).getId());
+                            problemDetail.setType(URI.create("http://localhost:8080/problems/post-not-found"));
+                            problemDetail.setInstance(URI.create(req.requestPath().toString()));
+                            problemDetail.setTitle("Out of Stock");
+
+                            return EntityResponse.fromObject(problemDetail)
+                                    .status(HttpStatus.NOT_FOUND)
+                                    .build();
+                        })
                 .build();
-    }
-
-    private static BiFunction<Throwable, ServerRequest, ServerResponse> error() {
-        return (e, req) -> {
-            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-            problemDetail.setProperty("id", ((ElementNotFoundException) e).getId());
-            problemDetail.setType(URI.create("http://localhost:8080/problems/post-not-found"));
-            problemDetail.setInstance(URI.create(req.requestPath().toString()));
-            problemDetail.setTitle("Out of Stock");
-
-            return EntityResponse.fromObject(problemDetail)
-                    .status(HttpStatus.NOT_FOUND)
-                    .build();
-        };
     }
 }
 
--- src/main/java/com/xavierbouclet/whiskies/api/handler/WhiskyHandler.java
+++ src/main/java/com/xavierbouclet/whiskies/api/handler/WhiskyHandler.java
@@ -4,12 +4,10 @@ import com.xavierbouclet.whiskies.api.exception.ElementNotFoundException;
 import com.xavierbouclet.whiskies.api.repository.WhiskyRepository;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import java.util.UUID;
-
 @Component
 public class WhiskyHandler {
 
@@ -19,12 +17,10 @@ public class WhiskyHandler {
         this.postRepository = postRepository;
     }
 
-    @GetMapping
     public ServerResponse findAll(ServerRequest serverRequest) {
         return  ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(postRepository.findAll());
     }
 
-    @GetMapping("/{id}")
     public ServerResponse findById(ServerRequest serverRequest) {
         UUID id=UUID.fromString(serverRequest.pathVariable("id"));
         return ServerResponse.ok().body(postRepository.findById(id).orElseThrow(()->new ElementNotFoundException(id)));
